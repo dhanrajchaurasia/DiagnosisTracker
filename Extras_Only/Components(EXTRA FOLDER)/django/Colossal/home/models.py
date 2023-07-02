351f17bf799a692f5131228777e8f2afea806a97
Dhanraj Chaurasia <81418590+dhanrajchaurasia@users.noreply.github.com>
2023-07-02 07:42:00 +0530

    Delete Extras_Only/Components(EXTRA FOLDER) directory

--- Extras_Only/Components(EXTRA FOLDER)/django/Colossal/home/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.db.models.fields import CharField
-
-# Create your models here.
-class Contact(models.Model):
-    email=models.CharField(max_length=122)
-    message=models.TextField()
